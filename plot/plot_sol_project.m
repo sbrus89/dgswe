04ef02f0d2ad5c95725e8b2bf74de5b10280f84a
Steven Brus <sbrus@nd.edu>
2014-08-20 11:23:32 -0400

    Changes to plot_sol.m
     - Renamed to plot_sol_project.m
    
     - Allow for different grid and solution directories
    
     - Modify to read in number of dof's for quads, but only use the
       triangle dof's
    
     - Comment out the plot printing

--- plot/plot_sol_project.m
+++ plot/plot_sol_project.m
@@ -12,13 +12,16 @@ nsnap = 100;
 %direc = '../LDG/';
 %direc = '../openmp/';
 direc = '~/dgswe/';
+grd_direc = '~/data-drive/galveston/dgswe/';
+sol_direc = '~/data-drive/galveston/dgswe/';
+grd_name = 'galveston.grd';
 
 % direc = '..';
 
-FramesFolder = strcat(direc,'output/velsnap') ;
-if ( exist(FramesFolder,'dir') == 0 ) 
-    mkdir(FramesFolder) ; 
-end
+% FramesFolder = strcat(direc,'output/velsnap') ;
+% if ( exist(FramesFolder,'dir') == 0 ) 
+%     mkdir(FramesFolder) ; 
+% end
 
 % f1 = figure('Position',[1360 667 560 420]);
 % f2 = figure('Position',[798 667 560 420]);
@@ -26,9 +29,9 @@ end
 h = figure('Position',[100 100 1500 1000]) ;
 
 % Open water column height, x momentum, and y momentum modal solution files
-fid_H = fopen([direc,'output/solution_H.d']);
-fid_Qx = fopen([direc,'output/solution_Qx.d']);
-fid_Qy = fopen([direc,'output/solution_Qy.d']);
+fid_H = fopen([sol_direc,'solution_H.d']);
+fid_Qx = fopen([sol_direc,'solution_Qx.d']);
+fid_Qy = fopen([sol_direc,'solution_Qy.d']);
 
 % Read in grid name
 % name = fscanf(fid_H,' %s ', 1);
@@ -39,18 +42,21 @@ fid_Qy = fopen([direc,'output/solution_Qy.d']);
 line = fgetl(fid_H);
 line = fgetl(fid_Qx);
 line = fgetl(fid_Qy);
-name = 'grids/inlet2.grd';
 
 % Read in grid file
-[EToV,VX,HB,~,~,~] = readfort14([direc,name]);
+[EToV,VX,HB,~,~,~] = readfort14([grd_direc,grd_name]);
 
 % Open L2 projection information file
-fid = fopen([direc,'output/projection.d']);
+fid = fopen([sol_direc,'projection.d']);
 
 N = fscanf(fid, '%g', 1); % number of degrees of freedom
 mL2 = fscanf(fid,' %g ',[N 3])'; % L2 projection matrix mL2(i,j) = integral of (phi_DG_basis(j)*phi_linear_nodal(i))
 mm = fscanf(fid,' %g ',[3 3])'; % linear nodal mass matrix
 fclose(fid);
+
+p = -3/2 + sqrt(9+8*N-8)/2;
+
+Nm = (p+1)^2;
 
 [ne ~] = size(EToV); % number of elements
 [nn ~] = size(VX); % number of nodes
@@ -128,17 +134,17 @@ while ~feof(fid_H) && snap < nsnap
 snap = snap + 1;
     
 th = fscanf(fid_H,' %g ', 1); % read in time
-H = fscanf(fid_H,' %g ', [ne N])'; % read in H solution at time t
+H = fscanf(fid_H,' %g ', [ne Nm])'; % read in H solution at time t
 
 tqx = fscanf(fid_Qx,' %g ', 1); % read in time
-Qx = fscanf(fid_Qx,' %g ', [ne N])'; % read in Qx solution at time t
+Qx = fscanf(fid_Qx,' %g ', [ne Nm])'; % read in Qx solution at time t
 
 t(snap) = fscanf(fid_Qy,' %g ', 1); % read in time
-Qy = fscanf(fid_Qy,' %g ', [ne N])'; % read in Qy solution at time t
+Qy = fscanf(fid_Qy,' %g ', [ne Nm])'; % read in Qy solution at time t
 
-prod_H = mL2*H; % compute product of elemental degrees of freedom and projection matrix
-prod_Qx = mL2*Qx;
-prod_Qy = mL2*Qy;
+prod_H = mL2*H(1:N,:); % compute product of elemental degrees of freedom and projection matrix
+prod_Qx = mL2*Qx(1:N,:);
+prod_Qy = mL2*Qy(1:N,:);
 
 % Assemble RHS vector
 B_H = zeros(nn,1);
@@ -168,7 +174,11 @@ projsol(:,2,:) = u;
 projsol(:,3,:) = v;
 projsol(:,4,:) = vel;
 
+velmax = max(max(vel));
+zmax = max(max(zeta));
+
 for tsnap = 1:snap
+%     figure 
     
     disp(['Time snap: ',num2str(tsnap),'/',num2str(snap)])
     [day hr min sec] = s2dhms(t(tsnap));
@@ -215,7 +225,7 @@ for tsnap = 1:snap
 
     pause(.01)
 
-    print('-r300','-dpng',sprintf('%s/surf%04d',FramesFolder,tsnap)) ;
+%     print('-r300','-dpng',sprintf('%s/surf%04d',FramesFolder,tsnap)) ;
     
 end
 
