9895464abcf05b0de1f428f2ce34edf3dfcfb5b3
Steven Brus <sbrus@nd.edu>
2014-08-20 10:26:09 -0400

    Updates to convert2dm.py
     - Add read/write of boundary conditions
    
       * 0 represents an open boundary
       * anything else < 100 is a land boundary
       * anything > 100 is ignored
    
     - Handle 9 node quads and 6 node triangles

--- grids/convert2dm.py
+++ grids/convert2dm.py
@@ -1,8 +1,8 @@
 import pprint
 
-#filename = "inlet_quad_generic.2dm"
-filename = "../grids/converge3_quad.2dm"
-fort14name = "../grids/converge3_quad.grd"
+filename = "/home/sbrus/data-drive/galveston/dgswe/galveston_curve.2dm"
+fort14name = "/home/sbrus/data-drive/galveston/dgswe/galveston_curve.grd"
+grid_name = "galveston_curve"
 
 f = open(filename)
 grid_data = f.read().split("\n")
@@ -14,6 +14,7 @@ ect = []
 xy = []
 ns = []
 bnd = []
+bc = {}
 for i in range(0,n):
   line = grid_data[i].split()
   print line
@@ -46,6 +47,32 @@ for i in range(0,n):
       data.append(line[3])
       data.append(line[4])
       xy.append(data)
+    elif line[0] == "E9Q":
+      data.append(line[1])
+      data.append("9")
+      data.append(line[2])
+      data.append(line[3])
+      data.append(line[4])
+      data.append(line[5])  
+      data.append(line[6])
+      data.append(line[7])
+      data.append(line[8])
+      data.append(line[9])
+      data.append(line[10])      
+      ect.append(data)            
+    elif line[0] == "E6T":
+      data.append(line[1])
+      data.append("6")
+      data.append(line[2])
+      data.append(line[3])
+      data.append(line[4])
+      data.append(line[5]) 
+      data.append(line[6]) 
+      data.append(line[7]) 
+      data.append("0")
+      data.append("0")
+      data.append("0")      
+      ect.append(data)      
     elif line[0] == "NS":
       if int(line[-2]) > 0:
         for j in range(1,len(line)):
@@ -55,6 +82,8 @@ for i in range(0,n):
           ns.append(str(abs(int(line[j]))))
         bnd.append(ns)
         ns = []
+    elif line[0] == "BC_VAL":
+      bc[line[2]] = line[3]
        
        
 ne = len(ect)
@@ -65,13 +94,13 @@ nope = 0
 neta = 0
 nbou = 0
 nvel = 0
-btype = [12,10,0,10]
 for i in range(0,nbnd):
+  seg = str(i+1)
   tbndnds = tbndnds + len(bnd[i])
-  if btype[i] == 0:
+  if bc[seg] == "0":
     nope = nope + 1
     neta = neta + len(bnd[i])
-  else:
+  elif bc[seg] != "0" and bc[seg] < "100":
     nbou = nbou + 1
     nvel = nvel + len(bnd[i])
     
@@ -82,8 +111,8 @@ pprint.pprint(bnd)
 
 f.close()
 
-fort14 = open(fort14name,"w")
-fort14.write("Quad Inlet\n")
+fort14 = open(fort14name,"w+")
+fort14.write(grid_name + "\n")
 fort14.write(str(ne) + " " +str(nn) + "\n")
 for i in range(0,nn):
   fort14.write( "   ".join(xy[i]) + "\n")
@@ -94,17 +123,19 @@ for i in range(0,ne):
 fort14.write(str(nope) + " = Number of open boundaries\n")  
 fort14.write(str(neta) + " = Total number of open boundary nodes\n")
 for i in range(0,nbnd):
-  if btype[i] == 0:
+  seg = str(i+1)
+  if bc[seg] == "0":
     nbndnds = len(bnd[i])    
-    fort14.write(str(nbndnds) + "  " +str(btype[i]) + "\n")
+    fort14.write(str(nbndnds) + "  " +str(bc[seg]) + "\n")
     for j in range(0,nbndnds):
       fort14.write(str(bnd[i][j]) + "\n")      
 fort14.write(str(nbou) + " = Number of land boundaries\n")
 fort14.write(str(nvel) + " = Total number of land boundary nodes\n")
 for i in range(0,nbnd):
-  if btype[i] != 0:
+  seg = str(i+1)
+  if bc[seg] != "0" and bc[seg] < "100":
     nbndnds = len(bnd[i])
-    fort14.write(str(nbndnds) + "  " +str(btype[i]) + "\n")
+    fort14.write(str(nbndnds) + "  " +str(bc[seg]) + "\n")
     for j in range(0,nbndnds):
       fort14.write(str(bnd[i][j]) + "\n")
     
