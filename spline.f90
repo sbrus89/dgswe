7eeb9b853ced101037d019c6b79844eb2a5c3864
Steven Brus <sbrus@nd.edu>
2014-09-05 14:54:09 -0400

    Modifications to spline.f90
     - Add spline tension
    
     - Compute the new x,y coordinates for non-vertex nodes on the
       boundary from the spline coefficients.
    
     - Write out the resulting mesh by calling the write_grid subroutine
    
     - Change some decimal numbers to d0.

--- spline.f90
+++ spline.f90
@@ -1,11 +1,14 @@
       PROGRAM spline
 
-      USE globals, ONLY: pres,nbou,fbseg,xy,fbnds,nepn,epn,vct,nverts,el_type
+      USE globals, ONLY: pres,ne,nn,ctp,xy,ect,vct,depth,nelnds, &
+                         nope,neta,obnds,nbou,nvel,fbseg,fbnds, &
+                         nepn,epn,nverts,el_type, &
+                         mnelnds
 
       IMPLICIT NONE
       INTEGER :: i,j,k,n,seg,sind,eind,num,qpts,btype
-      INTEGER :: el,eln,nd,led,n1ed1,n2ed1,n1bed,n2bed,nvert
-      REAL(pres) :: htest,mult,dt,t,x,y,qpt(2),norm
+      INTEGER :: el,eln,nd,ndn,led,n1ed1,n2ed1,n1bed,n2bed,nvert
+      REAL(pres) :: htest,mult,dt,t,tpt,x,y,r,sig
       REAL(pres) :: d1,d2,d3,t1,t2
       REAL(pres), ALLOCATABLE, DIMENSION(:) :: ax,bx,cx,dx
       REAL(pres), ALLOCATABLE, DIMENSION(:) :: ay,by,cy,dy
@@ -18,7 +21,9 @@
       CALL read_grid()
       
       CALL connect()
-
+      
+      sig = 1d0
+      
       num = 0
       DO seg = 1,nbou
         IF(fbseg(2,seg) == 10 .OR. fbseg(2,seg) == 11)THEN
@@ -42,7 +47,7 @@
           PRINT "(A,I5)", "Normal flow boundary nodes ",n
           PRINT "(A)", " "
 
-          PAUSE
+!           PAUSE
   
 
           ALLOCATE(ax(n),cx(n),bx(n-1),dx(n-1))
@@ -64,25 +69,47 @@
             k = k+1
           ENDDO
 
+! No tension         
+!           ! Set up matrix 
+!           Ml(1) = 0d0
+!           Md(1) = 1d0
+!           Mu(1) = 0d0
+!           DO i = 2,n-1
+!             Ml(i) = dt
+!             Md(i) = 4d0*dt
+!             Mu(i) = dt
+!           ENDDO
+!           Ml(n) = 0d0
+!           Md(n) = 1d0
+!           Mu(n) = 0d0
+! 
+!           ! Set up RHS
+!           v(1) = 0d0
+!           DO i = 2,n-1
+!             v(i) = 3d0/dt*(ax(i+1)-2d0*ax(i)+ax(i-1))
+!           ENDDO
+!           v(n) = 0d0
+
+! With tension
           ! Set up matrix 
-          Ml(1) = 0.0
-          Md(1) = 1.0
-          Mu(1) = 0.0
+          Ml(1) = 0d0
+          Md(1) = 1d0
+          Mu(1) = 0d0
           DO i = 2,n-1
-            Ml(i) = dt
-            Md(i) = 4.0*dt
-            Mu(i) = dt
+            Ml(i) = (2d0/sig**2)*(1d0/dt-sig/sinh(sig*dt))
+            Md(i) = (4d0/sig**2)*((sig*cosh(sig*dt))/sinh(sig*dt)-1d0/dt)
+            Mu(i) = (2d0/sig**2)*(1d0/dt-sig/sinh(sig*dt))
           ENDDO
-          Ml(n) = 0.0
-          Md(n) = 1.0
-          Mu(n) = 0.0
+          Ml(n) = 0d0
+          Md(n) = 1d0
+          Mu(n) = 0d0
 
           ! Set up RHS
-          v(1) = 0.0
+          v(1) = 0d0
           DO i = 2,n-1
-            v(i) = 3.0/dt*(ax(i+1)-ax(i)) - 3.0/dt*(ax(i)-ax(i-1))
+            v(i) = 1d0/dt*(ax(i+1)-2d0*ax(i)+ax(i-1))
           ENDDO
-          v(n) = 0.0
+          v(n) = 0d0
 
           ! Solve system for c coefficients, forward sweep
           DO i = 2,n
@@ -114,25 +141,47 @@
             k = k+1
           ENDDO
 
+! No tension          
+!           ! Set up matrix 
+!           Ml(1) = 0d0
+!           Md(1) = 1d0
+!           Mu(1) = 0d0
+!           DO i = 2,n-1
+!             Ml(i) = dt
+!             Md(i) = 4d0*dt
+!             Mu(i) = dt
+!           ENDDO
+!           Ml(n) = 0d0
+!           Md(n) = 1d0
+!           Mu(n) = 0d0
+! 
+!           ! Set up RHS
+!           v(1) = 0d0
+!           DO i = 2,n-1
+!             v(i) = 3d0/dt*(ay(i+1)-2d0*ay(i)+ay(i-1))
+!           ENDDO
+!           v(n) = 0d0
+          
+! With tension          
           ! Set up matrix 
-          Ml(1) = 0.0
-          Md(1) = 1.0
-          Mu(1) = 0.0
+          Ml(1) = 0d0
+          Md(1) = 1d0
+          Mu(1) = 0d0
           DO i = 2,n-1
-            Ml(i) = dt
-            Md(i) = 4.0*dt
-            Mu(i) = dt
+            Ml(i) = (2d0/sig**2)*(1d0/dt-sig/sinh(sig*dt))
+            Md(i) = (4d0/sig**2)*((sig*cosh(sig*dt))/sinh(sig*dt)-1d0/dt)
+            Mu(i) = (2d0/sig**2)*(1d0/dt-sig/sinh(sig*dt))
           ENDDO
-          Ml(n) = 0.0
-          Md(n) = 1.0
-          Mu(n) = 0.0
-
+          Ml(n) = 0d0
+          Md(n) = 1d0
+          Mu(n) = 0d0       
+          
           ! Set up RHS
-          v(1) = 0.0
+          v(1) = 0d0
           DO i = 2,n-1
-            v(i) = 3.0/dt*(ay(i+1)-ay(i)) - 3.0/dt*(ay(i)-ay(i-1))
+            v(i) = 1d0/dt*(ay(i+1)-2d0*ay(i)+ay(i-1))
           ENDDO
-          v(n) = 0.0
+          v(n) = 0d0          
 
           ! Solve system for c coefficients, forward sweep
           DO i = 2,n
@@ -154,7 +203,7 @@
           ENDDO
           
           
-          
+          t = 0d0
           DO nd = 1,n-1
             n1bed = fbnds(nd,seg)
             n2bed = fbnds(nd+1,seg)    
@@ -174,6 +223,20 @@
                    
                    print*, "  ", eln
                    
+                   DO i = 1,ctp-1
+                     r = -1d0 + real(i,pres)*2d0/real(ctp,pres)
+                     tpt = .5d0*dt*(r + 1d0) + t
+                     
+                     x = ax(nd) + bx(nd)*(tpt-t) + cx(nd)*(tpt-t)**2 + dx(nd)*(tpt-t)**3
+                     y = ay(nd) + by(nd)*(tpt-t) + cy(nd)*(tpt-t)**2 + dy(nd)*(tpt-t)**3
+                     
+                     ndn = mod(led,nvert)*ctp+i+1
+                     
+                     xy(1,ect(ndn,eln)) = x
+                     xy(2,ect(ndn,eln)) = y
+                   ENDDO
+                   
+                   t = t + dt
                    EXIT elem
 
                 ENDIF
@@ -203,5 +266,8 @@
         ENDIF
 
       ENDDO
+      CLOSE(30)
+      
+      CALL write_grid()
 
       END PROGRAM spline
