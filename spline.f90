bb6574ca5639b62ae3069ca44eda5de3258cec7d
Steven Brus <sbrus@nd.edu>
2014-09-09 10:15:41 -0400

    Improve node placement when evaluating spline
     - Evaluating the parameter halfway between the segment interval
       causes poorly placed nodes along sharp edges
    
     - Parameter value is now found though by minimizing the distance
       to the midpoint of the segment using Newton iteration.

--- spline.f90
+++ spline.f90
@@ -9,7 +9,9 @@
       INTEGER :: i,j,k,n,seg,sind,eind,num,qpts,btype
       INTEGER :: el,eln,nd,ndn,led,n1ed1,n2ed1,n1bed,n2bed,nvert
       REAL(pres) :: htest,mult,dt,t,tpt,x,y,r,sig
-      REAL(pres) :: d1,d2,d3,t1,t2
+      REAL(pres) :: d1,d2,d3,t1,t2,xm,ym
+      REAL(pres) :: n1x,n1y,n2x,n2y,edlen
+      REAL(pres) :: newton
       REAL(pres), ALLOCATABLE, DIMENSION(:) :: ax,bx,cx,dx
       REAL(pres), ALLOCATABLE, DIMENSION(:) :: ay,by,cy,dy
       REAL(pres), ALLOCATABLE, DIMENSION(:) :: Ml,Md,Mu,v
@@ -206,7 +208,23 @@
           t = 0d0
           DO nd = 1,n-1
             n1bed = fbnds(nd,seg)
-            n2bed = fbnds(nd+1,seg)    
+            n2bed = fbnds(nd+1,seg) 
+!             IF (nd == 1) THEN
+!               n3bed = fbnds(nd-1,seg)
+!             ELSE
+!               n3bed = fbnds(n-1,seg)            
+!             ENDIF
+            
+            n1x = xy(1,n1bed)
+            n1y = xy(2,n1bed)
+            
+            n2x = xy(1,n2bed)
+            n2y = xy(2,n2bed)
+
+            xm = .5d0*(n2x+n1x)
+            ym = .5d0*(n2y+n1y)
+                        
+            edlen = sqrt((n1x-n2x)**2+(n1y-n2y)**2)            
 
       elem: DO el = 1,nepn(n1bed)
               eln = epn(el,n1bed)
@@ -226,14 +244,17 @@
                    DO i = 1,ctp-1
                      r = -1d0 + real(i,pres)*2d0/real(ctp,pres)
                      tpt = .5d0*dt*(r + 1d0) + t
-                     
+
+                     tpt = newton(tpt,t,xm,ym,ax(nd),bx(nd),cx(nd),dx(nd),ay(nd),by(nd),cy(nd),dy(nd))
+
                      x = ax(nd) + bx(nd)*(tpt-t) + cx(nd)*(tpt-t)**2 + dx(nd)*(tpt-t)**3
                      y = ay(nd) + by(nd)*(tpt-t) + cy(nd)*(tpt-t)**2 + dy(nd)*(tpt-t)**3
                      
-                     ndn = mod(led,nvert)*ctp+i+1
-                     
+                     ndn = mod(led,nvert)*ctp+i+1                   
+
                      xy(1,ect(ndn,eln)) = x
                      xy(2,ect(ndn,eln)) = y
+                       
                    ENDDO
                    
                    t = t + dt
@@ -271,3 +292,51 @@
       CALL write_grid()
 
       END PROGRAM spline
+      
+      REAL(kind(0d0)) FUNCTION newton(told,t,xm,ym,ax,bx,cx,dx,ay,by,cy,dy)
+      
+      IMPLICIT NONE      
+      
+      INTEGER, PARAMETER :: pres = kind(0d0)
+      INTEGER :: it,maxit
+      REAL(pres) :: told,tnew,t
+      REAL(pres) :: xm,ym
+      REAL(pres) :: ax,bx,cx,dx,ay,by,cy,dy
+      REAL(pres) :: f,fp,fpp,g,gp,gpp
+      REAL(pres) :: d,dp,tol
+      
+      tol = 1d-4
+      maxit = 10000
+      
+iter: DO it = 1,maxit
+
+        f = ax + bx*(told-t) + cx*(told-t)**2 + dx*(told-t)**3
+        fp = bx + 2d0*cx*(told-t) + 3d0*dx*(told-t)**2
+        fpp = 2d0*cx + 6d0*dx*(told-t)
+        
+        g = ay + by*(told-t) + cy*(told-t)**2 + dy*(told-t)**3
+        gp = by + 2d0*cy*(told-t) + 3d0*dy*(told-t)**2
+        gpp = 2d0*cy + 6d0*dy*(told-t)
+        
+        d = 2d0*(f-xm)*fp + 2d0*(g-ym)*gp
+        dp = 2d0*fp**2 + 2d0*(f-xm)*fpp + 2d0*gp**2 + 2d0*(g-ym)*gpp
+        
+        tnew = told - d/dp
+        
+        IF (ABS(d) < tol) THEN
+          PRINT*, "iterations", it
+          EXIT iter
+        ENDIF
+        
+        told = tnew
+        
+      ENDDO iter
+      
+      IF (it == maxit) THEN
+        PRINT*, "MAX ITERATIONS EXCEEDED"
+      ENDIF
+      
+      newton = tnew
+      
+      RETURN 
+      END FUNCTION
