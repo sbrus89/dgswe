394de1fca95dc384135a73ef632f672cdcfb7930
Steven Brus <sbrus@nd.edu>
2015-05-27 18:07:29 -0400

    Stop tracking the version.F90 file
      - Since the Makefile modifies this file, it could be a pain to
        keep tracking it

--- src/version.F90
+++
@@ -1,9 +0,0 @@
-      SUBROUTINE version()
-
-      IMPLICIT NONE
-      
-      PRINT*, "Version (git SHA):" 
-      PRINT*, "1087761b68d17d3b507f4e817922900e3d0b3433 +" 
-      PRINT*, " "
- 
-      END SUBROUTINE version
