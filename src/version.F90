42f340cdd835c438785dbd32f973697d69b46c68
Steven Brus <sbrus@nd.edu>
2015-10-01 15:08:37 -0400

    Add modified files output to version.F90
     - Makefile calls git to find modified files and writes them
       in version.F90 before compilation

--- src/version.F90
+++ src/version.F90
@@ -1,21 +1,22 @@
       SUBROUTINE version()
       
-      USE globals, ONLY: gitSHA,gitBranch,compiler_version,compiler_flags
+      USE globals, ONLY: gitSHA,gitBranch,compiler_version,compiler_flags,modified_files
       USE messenger2, ONLY: myrank
 
       IMPLICIT NONE
       
       gitBranch = "quadtri_mixed_mpi" 
-      gitSHA = "a90514ca26982fbef203aab77c5aa0913acaa9c3 +" 
+      gitSHA = "022fffcb5a6cbee435671f56582a9a10a81adbc0 +" 
       compiler_version = "ifort version 14.0.0" 
       compiler_flags = "-O3 -xHost -Iodir_dgswe/" 
-      
+      modified_files = "../src/globals.F90 ../src/version.F90 Makefile" 
       
       IF (myrank == 0) THEN     
             
         PRINT*, "Version Information"
         PRINT*, "  Branch: ", TRIM(ADJUSTL(gitBranch))
         PRINT*, "  SHA: ", TRIM(ADJUSTL(gitSHA)) 
+        PRINT*, "  Modified files: ", TRIM(ADJUSTL(modified_files))
         PRINT*, " "
         PRINT*, "Compiler Information"
         PRINT*, "  Version: ", TRIM(ADJUSTL(compiler_version))
