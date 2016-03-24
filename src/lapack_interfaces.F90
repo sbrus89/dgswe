c2ca282daa9abc002c032f5bcbc6e36436ab1066
Steven Brus <sbrus@nd.edu>
2016-03-24 14:19:44 -0400

    Clean up duplicate files
     - remove the duplicate version.f90, kdtree2.F, and lapack_interfaces.F90
       files from the rimls and spline src directories
    
     - the spline lapack_interfaces.f90 file had an additional interface
       for DGTSV that was incorporated to the dgswe file

--- src/lapack_interfaces.F90
+++ src/lapack_interfaces.F90
@@ -166,6 +166,55 @@ MODULE lapack_interfaces
                                                                    
         
         END SUBROUTINE DGETRI
+        
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                      
+        
+        SUBROUTINE DGTSV(n, nrhs, dl, d, du, b, ldb, info)
+        
+          !  DGTSV  solves the equation
+          !
+          !     A*X = B,
+          !
+          !  where A is an n by n tridiagonal matrix, by Gaussian elimination with
+          !  partial pivoting.
+          !
+          !  Note that the equation  A**T*X = B  may be solved by interchanging the
+          !  order of the arguments DU and DL.        
+        
+          INTEGER, INTENT(IN) :: n                                   ! The order of the matrix A.  N >= 0.
+        
+          INTEGER, INTENT(IN) :: nrhs                                ! The number of right hand sides, i.e., the number of columns
+                                                                     ! of the matrix B.  NRHS >= 0.
+                                                                   
+          DOUBLE PRECISION, DIMENSION(*), INTENT(INOUT) :: dl        ! dimension (N-1)
+                                                                     ! On entry, DL must contain the (n-1) sub-diagonal elements of A.
+                                                                     ! On exit, DL is overwritten by the (n-2) elements of the
+                                                                     ! second super-diagonal of the upper triangular matrix U from
+                                                                     ! the LU factorization of A, in DL(1), ..., DL(n-2).
+                                                                   
+          DOUBLE PRECISION, DIMENSION(*), INTENT(INOUT) :: d         ! dimension (N)
+                                                                     ! On entry, D must contain the diagonal elements of A.
+                                                                     ! On exit, D is overwritten by the n diagonal elements of U.
+                                                                   
+          DOUBLE PRECISION, DIMENSION(*), INTENT(INOUT) :: du        ! dimension (N-1)
+                                                                     ! On entry, DU must contain the (n-1) super-diagonal elements of A.
+                                                                     ! On exit, DU is overwritten by the (n-1) elements of the first
+                                                                     ! super-diagonal of U.
+                                                                   
+          DOUBLE PRECISION, DIMENSION(ldb,*), INTENT(INOUT) :: b     ! dimension (LDB,NRHS)
+                                                                     ! On entry, the N by NRHS matrix of right hand side matrix B.
+                                                                     ! On exit, if INFO = 0, the N by NRHS solution matrix X.
+                                                                   
+          INTEGER, INTENT(IN) :: ldb                                 ! The leading dimension of the array B.  LDB >= max(1,N).
+        
+          INTEGER, INTENT(OUT) :: info                               ! = 0: successful exit
+                                                                     ! < 0: if INFO = -i, the i-th argument had an illegal value
+                                                                     ! > 0: if INFO = i, U(i,i) is exactly zero, and the solution
+                                                                     !      has not been computed.  The factorization has not been
+                                                                     !      completed unless i = N.
+          
+        END SUBROUTINE DGTSV            
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   
