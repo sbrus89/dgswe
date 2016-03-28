a6ad320bcda7ba67ec6478370762bb13ba96f7da
Steven Brus <sbrus@nd.edu>
2016-03-28 13:51:26 -0400

    Get rid of local variables in rimls connect.f90

--- rimls/src/connect.f90
+++ rimls/src/connect.f90
@@ -5,32 +5,10 @@
                          
 
       IMPLICIT NONE
-      INTEGER :: el,el1,el2,led1,led2,i,seg,m,ged,ed,ed1,ed2,nd,ne,nn,ned
-      INTEGER :: n1,n2,nnds
-      INTEGER :: n1ed1,n2ed1,n1ed2,n2ed2
-      INTEGER :: n1bed,n2bed
-      INTEGER :: segtype
-      INTEGER :: alloc_status
-      INTEGER :: found
-      INTEGER :: nvert1,nvert2,nvert
-      INTEGER :: el_in,el_ex
-      REAL(rp) :: x1,x2,x3,y1,y2,y3
       
       TYPE(grid) :: mesh
-      
-      INTEGER, ALLOCATABLE, DIMENSION(:) :: nfbnd_temp      
-      INTEGER, ALLOCATABLE, DIMENSION(:,:) :: ged2nn_temp, ged2el_temp, ged2led_temp ! temporary arrays for edge connectivity     
-      INTEGER, ALLOCATABLE, DIMENSION(:,:) :: edflag
-      INTEGER :: mnepn ! maximum number of elements per node
+         
 
-      
-      ne = mesh%ne
-      nn = mesh%nn
-
-      ALLOCATE(ged2nn_temp(2,3*ne),ged2el_temp(2,3*ne),ged2led_temp(2,3*ne),STAT = alloc_status)
-      IF(alloc_status /= 0) THEN
-        PRINT*, 'Allocation error: ged2nn_temp,ged2el_temp,ged2led_temp'
-      ENDIF 
 
       CALL elements_per_node(mesh%ne,mesh%nn,nverts, &
                              mesh%el_type,mesh%ect, &
