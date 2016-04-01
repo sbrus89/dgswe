fd7196487e8bc2e02979c71277a2a574d199cebc
Steven Brus <sbrus@nd.edu>
2016-04-01 01:17:38 -0400

    Use min_edge_length subroutine in spline code

--- spline/src/connect.f90
+++ spline/src/connect.f90
@@ -44,41 +44,10 @@
       CALL print_connect_info(mesh%mnepn,mesh%ned,mesh%nied,mesh%nobed,mesh%nfbed,mesh%nnfbed,mesh%nred)                                  
 
 
-      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      ! find minimum edge length
-      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-      ALLOCATE(mesh%edlen(mesh%ned),mesh%minedlen(mesh%ne))
-      
-      mesh%minedlen = 1d6
-      DO ed = 1,mesh%ned
-        n1 = mesh%ged2nn(1,ed)
-        n2 = mesh%ged2nn(2,ed)                
-        
-        x1 = mesh%xy(1,n1)
-        x2 = mesh%xy(1,n2)
-        
-        y1 = mesh%xy(2,n1)
-        y2 = mesh%xy(2,n2)
-        
-        mesh%edlen(ed) = sqrt((x2-x1)**2 + (y2-y1)**2)        
-        
-        el_in = mesh%ged2el(1,ed)
-        el_ex = mesh%ged2el(2,ed)
-        
-        IF (mesh%edlen(ed) < mesh%minedlen(el_in)) THEN
-          mesh%minedlen(el_in) = mesh%edlen(ed)
-        ENDIF
-        
-        IF (el_ex > 0 ) THEN
-          IF (mesh%edlen(ed) < mesh%minedlen(el_ex)) THEN
-            mesh%minedlen(el_ex) = mesh%edlen(ed)
-          ENDIF          
-        ENDIF
-        
-        
-      ENDDO
+      CALL min_edge_length(mesh%ned,mesh%ne,mesh%xy, &
+                           mesh%ged2el,mesh%ged2nn, &
+                           mesh%edlen,mesh%minedlen)
+            
       
       
       PRINT "(A)", "---------------------------------------------"
