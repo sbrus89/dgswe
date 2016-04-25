85009a65dbf98390dc6ba18012f45df0ced543bf
Steven Brus <sbrus@nd.edu>
2016-04-25 18:02:28 -0400

    Improve bounding box plots in rimls plot_nodes.m
     - Allow the bounding box to be applied to the plot of the base
       nodes, base centers, eval evaluation points, eval grid curved
       element node coordinates, and eval grid elements.
    
     - Add filter_ect.m which returns the ect information for the
       verticies inside the bounding box.

---
+++ rimls/plot/filter_ect.m
@@ -0,0 +1,29 @@
+function [ect_new,nelnds_new] = filter_ect(ect,nelnds,in)
+
+ne = length(ect);
+nn = length(in);
+
+ect_tmp = zeros(ne,3);
+nelnds_tmp = zeros(ne,1);
+ndnums_new = zeros(nn,1);
+
+i = 0;
+for nd = 1:nn
+    if (in(nd) == 1) 
+        i = i +1;
+        ndnums_new(nd) = i;
+    end
+end 
+
+i = 0;
+for el = 1:ne
+   nv = nelnds(el);
+   if all(in(ect(el,1:nv))) 
+      i = i + 1;
+      ect_tmp(i,:) = ndnums_new(ect(el,1:nv));
+      nelnds_tmp(i) = nv;
+   end
+end
+
+ect_new = ect_tmp(1:i,:);
+nelnds_new = nelnds_tmp(1:i);
