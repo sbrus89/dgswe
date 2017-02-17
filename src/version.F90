9dbcf99a2fec352c21e3e3896b3c513a309fd947
Steven Brus <sbrus@nd.edu>
2017-02-17 15:22:45 -0500

    Removed modified files from version.F90
     - Causes compilation issues if to many files are listed

--- src/version.F90
+++ src/version.F90
@@ -23,12 +23,12 @@
       INTEGER :: length,ind,sind,eind        
 
       gitBranch = "master" 
-      gitSHA = "e5cd3c8f92010fbbfdf5a991b8ae13628f14ba32 +" 
+      gitSHA = "2ce9a54fe2d98aa2572d9b99d8801318f3be6522 +" 
       compiler_version = "Using built-in specs. COLLECT_GCC=gfortran" 
       compiler_flags = "-O2 -Iodir_dgswe/" 
-      modified_files = "../src/allocation.F90 ../src/grid_file_mod.F90 ../src/kdtree2.F ../src/read_forcing.F90 ../src/read_input.F90 Makefile" 
-      compile_date = "Thu Jan 26 23:24:09 EST 2017" 
-      host = "sbrus-laptop" 
+      modified_files = "" 
+      compile_date = "Fri Feb 17 15:20:34 EST 2017" 
+      host = "chl-tilos" 
 
       
       length = LEN(TRIM(ADJUSTL(modified_files)))
@@ -37,18 +37,18 @@
       CALL directed_output("Version Information",unit)
       CALL directed_output("  Branch: "//TRIM(ADJUSTL(gitBranch)),unit)
       CALL directed_output("  SHA: "//TRIM(ADJUSTL(gitSHA)),unit)
-      CALL directed_output("  Modified files: ",unit)
-
-        
-      sind = 1        
-      ind = INDEX(modified_files," ")        
-      eind = ind
-      DO WHILE (ind > 0)
-        CALL directed_output("    " //TRIM(ADJUSTL(modified_files(sind:eind))),unit)
-        sind = eind+1
-        ind = INDEX(modified_files(sind:length)," ")
-        eind = eind + ind
-      ENDDO
+!       CALL directed_output("  Modified files: ",unit)
+! 
+!         
+!       sind = 1        
+!       ind = INDEX(modified_files," ")        
+!       eind = ind
+!       DO WHILE (ind > 0)
+!         CALL directed_output("    " //TRIM(ADJUSTL(modified_files(sind:eind))),unit)
+!         sind = eind+1
+!         ind = INDEX(modified_files(sind:length)," ")
+!         eind = eind + ind
+!       ENDDO
         
       CALL directed_output(" ",unit)
       CALL directed_output("Compiler Information",unit)
