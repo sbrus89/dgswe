457d96e79737f967eb97b6756221a4a3ed83f027
Steven Brus <sbrus@nd.edu>
2017-02-18 18:43:50 -0500

    Bug fixes for curvilinear elements in dgswe
     - Two fixes in boundary_edge_land.F90
    
     1) Only zero out the quadrature point locations for the straight-sided
        edges in the qpt arrays. This was being done for all the curved
        edge quadrature points, causeing the straight-sided information
        to be overwritten.
    
     2) Compute the eddy viscosity auxiliary variables at the curved
        edge quadrature points in boundary_edge_land(). The straight-sided
        values from the qpt arrays were being used.

--- src/boundary_edge_land.F90
+++ src/boundary_edge_land.F90
@@ -7,6 +7,7 @@
                          Zqpt,Qxqpt,Qyqpt, &
                          rhsZ,rhsQx,rhsQy, &
                          phie,phie_int, &
+                         Exx,Eyy,Exy,Eyx, &                         
                          Exxqpt,Eyyqpt,Exyqpt,Eyxqpt
                          
       USE read_dginp, ONLY: ctp                         
@@ -99,27 +100,31 @@
 
             Z_in = Z(el_in,1)
             Qx_in = Qx(el_in,1)
-            Qy_in = Qy(el_in,1)
+            Qy_in = Qy(el_in,1)            
+            Exx_in = Exx(el_in,1)
+            Eyy_in = Eyy(el_in,1)
+            Exy_in = Exy(el_in,1)
+!             Eyx_in = Eyx(el_in,1)
             DO dof = 2,ndof(et)
               Z_in  = Z_in  + Z(el_in,dof)*phie(dof,gp_in,et)
               Qx_in = Qx_in + Qx(el_in,dof)*phie(dof,gp_in,et)
               Qy_in = Qy_in + Qy(el_in,dof)*phie(dof,gp_in,et)
+              
+              Exx_in = Exx_in + Exx(el_in,dof)*phie(dof,gp_in,et)
+              Eyy_in = Eyy_in + Eyy(el_in,dof)*phie(dof,gp_in,et)
+              Exy_in = Exy_in + Exy(el_in,dof)*phie(dof,gp_in,et)
+!               Eyx_in = Eyx_in + Eyx(el_in,dof)*phie(dof,gp_in,et)
             ENDDO            
               
             Z_ex = Z_in
             Qx_ex = Qx_in*(ny2-nx2) - 2d0*nxny*Qy_in
             Qy_ex = Qy_in*(nx2-ny2) - 2d0*nxny*Qx_in
 
-            CALL numerical_flux(Qx_in,Qy_in,Z_in,Qx_ex,Qy_ex,Z_ex,hb,nx,ny,sp,Qxhat,Qyhat,Zhat)
-            
-            Exx_in = Exxqpt(el_in,gp_in)
-            Eyy_in = Eyyqpt(el_in,gp_in)
-            Exy_in = Exyqpt(el_in,gp_in)
-!             Eyx_in = Eyxqpt(el_in,gp_in)            
+            CALL numerical_flux(Qx_in,Qy_in,Z_in,Qx_ex,Qy_ex,Z_ex,hb,nx,ny,sp,Qxhat,Qyhat,Zhat)                      
            
             Zhat  = detJe(ged,pt)*Zhat              
             Qxhat = detJe(ged,pt)*(Qxhat - nx*Exx_in - ny*Exy_in)
-            Qyhat = detJe(ged,pt)*(Qyhat - nx*Exy_in - ny*Eyy_in)
+            Qyhat = detJe(ged,pt)*(Qyhat - nx*Exy_in - ny*Eyy_in)                  
 
 !             Qxhat = detJe(ged,pt)*(Qxhat - nx*Exx_in - ny*Exy_in)
 !             Qyhat = detJe(ged,pt)*(Qyhat - nx*Eyx_in - ny*Eyy_in)
@@ -128,13 +133,18 @@
               rhsZ(el_in,l)  = rhsZ(el_in,l)  - Zhat*phie_int(l,gp_in,et)
               rhsQx(el_in,l) = rhsQx(el_in,l) - Qxhat*phie_int(l,gp_in,et)
               rhsQy(el_in,l) = rhsQy(el_in,l) - Qyhat*phie_int(l,gp_in,et)                   
-            ENDDO  
+            ENDDO                                       
+              
+          ENDDO
+          
+          DO pt = 1,nqpte(1)
+            gp_in = (led_in-1)*nqpte(1) + pt
             
             Zqpt(el_in,gp_in) = 0d0
             Qxqpt(el_in,gp_in) = 0d0
-            Qyqpt(el_in,gp_in) = 0d0          
-              
+            Qyqpt(el_in,gp_in) = 0d0            
           ENDDO
+          
         ENDDO
 !$OMP end do
       ENDIF     
@@ -154,7 +164,7 @@
                          el_type,nqpte,ndof, &
                          nx_pt,ny_pt,Spe,detJe, &
                          Qx,Qy,phie,phie_int, &                         
-                         Qxqpt,Qyqpt, &
+                         Zqpt,Qxqpt,Qyqpt, &
                          Exxqpt,Eyyqpt,Exyqpt,Eyxqpt, &
                          rhsExx,rhsEyy,rhsExy,rhsEyx
                          
@@ -253,15 +263,19 @@
               rhsExx(el_in,l) = rhsExx(el_in,l) + Exx_hat*phie_int(l,gp_in,et)
               rhsEyy(el_in,l) = rhsEyy(el_in,l) + Eyy_hat*phie_int(l,gp_in,et)
               rhsExy(el_in,l) = rhsExy(el_in,l) + Exy_hat*phie_int(l,gp_in,et)   
-              rhsEyx(el_in,l) = rhsEyx(el_in,l) + Eyx_hat*phie_int(l,gp_in,et)                
+!               rhsEyx(el_in,l) = rhsEyx(el_in,l) + Eyx_hat*phie_int(l,gp_in,et)                
             ENDDO  
+
+          ENDDO
+          
+          DO pt = 1,nqpte(1)
+            gp_in = (led_in-1)*nqpte(1) + pt
             
             Exxqpt(el_in,gp_in) = 0d0
             Eyyqpt(el_in,gp_in) = 0d0         
             Exyqpt(el_in,gp_in) = 0d0  
-!             Eyxqpt(el_in,gp_in) = 0d0              
-          
-          ENDDO
+!             Eyxqpt(el_in,gp_in) = 0d0           
+          ENDDO          
         ENDDO
       
       ENDIF
