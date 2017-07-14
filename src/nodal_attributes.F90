07f2459c7466afbfbc5d78e8bde4f4fec081cfe3
Steven Brus <sbrus@nd.edu>
2017-07-14 16:22:30 -0400

    Update fort.13 utilities
     - create_fort13.F90 is set up to merge the SL18TX33 and PRVI
       fort.13 data into the DHW Oster grid.

--- src/nodal_attributes.F90
+++ src/nodal_attributes.F90
@@ -1,6 +1,7 @@
       MODULE nodal_attributes_mod
       
       USE globals, ONLY: rp
+      USE kdtree2_module      
       
       IMPLICIT NONE
       
@@ -20,6 +21,7 @@
         
         INTEGER, DIMENSION(:), ALLOCATABLE :: nvals
         INTEGER, DIMENSION(:), ALLOCATABLE :: nndns
+        INTEGER, DIMENSION(:,:), ALLOCATABLE :: ndns
         INTEGER, DIMENSION(:), ALLOCATABLE :: attr_index        
         REAL(rp), DIMENSION(:), ALLOCATABLE :: default_val
         REAL(rp), DIMENSION(:,:), ALLOCATABLE :: defined_val
@@ -27,6 +29,33 @@
 
 
       CONTAINS
+      
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+
+      SUBROUTINE print_fort13_info(fort13)
+      
+      IMPLICIT NONE
+      
+      TYPE(type13), INTENT(IN) :: fort13      
+      
+      INTEGER :: attr
+      INTEGER :: n
+      INTEGER :: k
+      
+      n = 0
+      DO attr = 1,fort13%nattr
+        PRINT*, TRIM(fort13%name(attr)%line)
+        PRINT("(12(F15.5))"), (fort13%default_val(n+k), k = 1,fort13%nvals(attr))
+        PRINT*, fort13%nndns(attr)
+        PRINT("(12(F15.5))"), (MAXVAL(fort13%defined_val(:,n+k)), k = 1,fort13%nvals(attr))
+        PRINT("(12(F15.5))"), (MINVAL(fort13%defined_val(:,n+k)), k = 1,fort13%nvals(attr))        
+        n = n + fort13%nvals(attr)
+        PRINT*, "" 
+      ENDDO      
+      
+      RETURN
+      END SUBROUTINE print_fort13_info
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
@@ -76,13 +105,11 @@
         STOP       
       ENDIF      
       
-      PRINT*, "reading header"
       OPEN(UNIT=13,FILE=file_name)
       READ(13,"(A100)") grid_name      
       READ(13,*) nn
       READ(13,*) nattr
-      
-      PRINT*, "reading values"
+
       n = 0
       ALLOCATE(default_val(30))
       ALLOCATE(name(nattr),units(nattr))
@@ -94,10 +121,7 @@
         READ(13,*) units(attr)%line
         READ(13,*) nvals(attr)
         
-        PRINT*, name(attr)%line, units(attr)%line, nvals(attr)
-        
         READ(13,*) (default_val(n+k), k = 1,nvals(attr))
-        PRINT*, (default_val(n+k), k = 1,nvals(attr))
         attr_index(attr) = n+1
         n = n + nvals(attr)
               
@@ -155,14 +179,11 @@
       DO attr = 1,nattr
       
         READ(13,*) check
-        PRINT*, check
-        PRINT*, name(attr)%line
         IF (check /= name(attr)%line) THEN
           PRINT*, "Matching error in fort.13"
           STOP
         ENDIF
         READ(13,*) nndns(attr)
-        PRINT*, nndns(attr)
         
         DO i = 1,nndns(attr)
           READ(13,*) nd, (defined_val(nd,n+k), k = 1,nvals(attr))
@@ -179,28 +200,33 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
  
-      SUBROUTINE find_mode()
-      
-      IMPLICIT NONE
-            
-      DO attr = 1,nattr
-        n = 0
-        DO k = 1,nvals(attr)
-          DO nd = 1,nn
-            defined_val(nd,n+k)           
-          ENDDO
-        ENDDO        
-        n = n + nvals(attr)
-      ENDDO
-      
-      RETURN
-      END SUBROUTINE find_mode
+!       SUBROUTINE find_mode()
+!       
+!       IMPLICIT NONE
+!             
+!       DO attr = 1,nattr
+!         n = 0
+!         DO k = 1,nvals(attr)
+!           DO nd = 1,nn
+!             defined_val(nd,n+k)           
+!           ENDDO
+!         ENDDO        
+!         n = n + nvals(attr)
+!       ENDDO
+!       
+!       RETURN
+!       END SUBROUTINE find_mode
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 
-      SUBROUTINE write_fort13()
+      SUBROUTINE write_fort13(fort13)
       
+      TYPE(type13), INTENT(IN) :: fort13
+      
+      CALL write_default_attribute(fort13%file_name,fort13%grid_name,fort13%nn,fort13%nattr, &
+                                   fort13%name,fort13%units,fort13%nvals,fort13%default_val)
+      CALL write_defined_attribute(fort13%nattr,fort13%name,fort13%nvals,fort13%nndns,fort13%ndns,fort13%defined_val)
       
       RETURN
       END SUBROUTINE write_fort13
@@ -208,8 +234,41 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 
-      SUBROUTINE write_default_attribute()
+      SUBROUTINE write_default_attribute(file_name,grid_name,nn,nattr,name,units,nvals,default_val)
       
+      IMPLICIT NONE
+      
+      CHARACTER(*), INTENT(IN) :: file_name
+      CHARACTER(*), INTENT(IN) :: grid_name
+      INTEGER, INTENT(IN) :: nn
+      INTEGER, INTENT(IN) :: nattr
+      TYPE(char_array), DIMENSION(:), ALLOCATABLE, INTENT(IN) :: name
+      TYPE(char_array), DIMENSION(:), ALLOCATABLE, INTENT(IN) :: units
+      INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(IN) :: nvals    
+      REAL(rp), DIMENSION(:), ALLOCATABLE, INTENT(IN) :: default_val      
+      
+      INTEGER :: attr
+      INTEGER :: k
+      INTEGER :: n
+      
+      PRINT*, "writing header"
+      OPEN(UNIT=13,FILE=file_name)
+      WRITE(13,"(A80)") grid_name      
+      WRITE(13,*) nn
+      WRITE(13,*) nattr
+      
+      PRINT*, "writing values"
+      n = 0
+      DO attr = 1,nattr
+      
+        WRITE(13,*) TRIM(name(attr)%line)
+        WRITE(13,*) TRIM(units(attr)%line)
+        WRITE(13,*) nvals(attr)
+        
+        WRITE(13,*) (default_val(n+k), k = 1,nvals(attr))
+        n = n + nvals(attr)
+              
+      ENDDO      
       
       RETURN
       END SUBROUTINE write_default_attribute
@@ -217,7 +276,36 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-     SUBROUTINE write_defined_attribute()
+      SUBROUTINE write_defined_attribute(nattr,name,nvals,nndns,ndns,defined_val)
+     
+      IMPLICIT NONE
+     
+      INTEGER, INTENT(IN) :: nattr
+      TYPE(char_array), DIMENSION(:), INTENT(IN) :: name
+      INTEGER, DIMENSION(:), INTENT(IN) :: nvals
+      INTEGER, DIMENSION(:), INTENT(IN) :: nndns
+      INTEGER, DIMENSION(:,:), INTENT(IN) :: ndns 
+      REAL(rp), DIMENSION(:,:), INTENT(IN) :: defined_val
+      
+      INTEGER :: attr
+      INTEGER :: i,k,nd
+      INTEGER :: n
+      
+      n = 0
+      DO attr = 1,nattr
+      
+        WRITE(13,*) TRIM(name(attr)%line)
+        WRITE(13,*) nndns(attr)
+        
+        DO i = 1,nndns(attr)
+          nd = ndns(i,attr)
+          WRITE(13,*) nd, (defined_val(nd,n+k), k = 1,nvals(attr))
+        ENDDO
+        n = n + nvals(attr)
+      
+      ENDDO
+               
+      CLOSE(13)     
      
      
      RETURN
@@ -225,5 +313,114 @@
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     
+
+      SUBROUTINE determine_non_default_nodes(nn,val,default_val,nndns,ndns,defined_val)
+      
+      IMPLICIT NONE
+      
+      INTEGER, INTENT(IN) :: nn
+      REAL(rp), DIMENSION(:), INTENT(IN) :: val
+      REAL(rp), INTENT(IN) :: default_val      
+      INTEGER, INTENT(OUT) :: nndns
+      INTEGER, DIMENSION(:), INTENT(OUT) :: ndns
+      REAL(rp), DIMENSION(:), INTENT(OUT) :: defined_val
+      
+      
+      INTEGER :: nd
+      
+      
+      nndns = 0
+      DO nd = 1,nn
+        IF (abs(val(nd)-default_val) > 1d-12) THEN
+          
+          nndns = nndns + 1
+          defined_val(nndns) = val(nd)
+          ndns(nndns) = nd
+          
+        ENDIF
+      ENDDO
+      
+      RETURN
+      END SUBROUTINE determine_non_default_nodes
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      SUBROUTINE create_neighbor_table(nn_search,xy_search,nn_find,xy_find,neighbor_table)
+      
+      IMPLICIT NONE
+      
+      INTEGER, INTENT(IN) :: nn_search
+      REAL(rp), DIMENSION(:,:), INTENT(IN) :: xy_search
+      INTEGER, INTENT(IN) :: nn_find
+      REAL(rp), DIMENSION(:,:), INTENT(IN) :: xy_find
+      INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(OUT) :: neighbor_table
+      
+      TYPE(kdtree2), POINTER :: tree_xy      
+      TYPE(kdtree2_result), ALLOCATABLE, DIMENSION(:) :: closest   
+      REAL(rp) :: xy(2)    
+      INTEGER :: nd
+      
+      PRINT*, "Building kd-tree"
+      tree_xy => kdtree2_create(xy_search(1:2,1:nn_search), rearrange=.true., sort=.true.)
+      ALLOCATE(closest(1))          
+      PRINT*, "done"
+     
+      ALLOCATE(neighbor_table(nn_find))
+      DO nd = 1,nn_find
+      
+        xy(1) = xy_find(1,nd)
+        xy(2) = xy_find(2,nd)
+        
+        CALL kdtree2_n_nearest(tp=tree_xy,qv=xy,nn=1,results=closest) 
+        
+        neighbor_table(nd) = closest(1)%idx
+
+      ENDDO      
+      
+      RETURN
+      END SUBROUTINE
+      
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      SUBROUTINE nearest_neighbor_interpolation(nn,neighbor_table,val,default_val,nndns,ndns,defined_val)
+      
+      IMPLICIT NONE
+      
+      INTEGER, INTENT(IN) :: nn
+      INTEGER, DIMENSION(:), INTENT(IN) :: neighbor_table
+      REAL(rp), DIMENSION(:), INTENT(IN) :: val
+      REAL(rp), INTENT(IN) :: default_val
+      INTEGER, INTENT(OUT) :: nndns
+      INTEGER, DIMENSION(:), INTENT(OUT) :: ndns
+      REAL(rp), DIMENSION(:), INTENT(OUT) :: defined_val
+      
+      INTEGER :: nd
+      INTEGER :: clnd
+      
+      nndns = 0
+      DO nd = 1,nn
+      
+        clnd = neighbor_table(nd)
+        
+        IF (abs(val(clnd)-default_val) > 1d-12) THEN
+          nndns = nndns + 1
+          ndns(nndns) = nd
+          defined_val(nd) = val(clnd)
+        ENDIF
+          
+      ENDDO      
+      
+      
+      
+      RETURN
+      END SUBROUTINE nearest_neighbor_interpolation
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
      
       END MODULE nodal_attributes_mod
--- util/create_fort13.F90
+++ util/create_fort13.F90
@@ -1,16 +1,149 @@
       PROGRAM create_fort13
 
-      USE nodal_attributes_mod, ONLY: type13,read_fort13
+      USE globals, ONLY: rp      
+      USE nodal_attributes_mod, ONLY: type13,read_fort13,write_fort13,apply_default_attributes, &
+                                      create_neighbor_table,nearest_neighbor_interpolation, &
+                                      determine_non_default_nodes,print_fort13_info
+      USE grid_file_mod, ONLY: grid_type,read_grid
+      USE kdtree2_module
 
       IMPLICIT NONE
 
-      TYPE(type13) :: old_grid
+      TYPE(grid_type) :: old14     
+      TYPE(type13) :: old13
+      
+      TYPE(grid_type) :: prvi14
+      TYPE(type13) :: prvi13
+
+      TYPE(grid_type) :: new14
+      TYPE(grid_type) :: manning14
+      TYPE(grid_type) :: submerge14       
+      TYPE(type13) :: new13
+
+      TYPE(kdtree2), POINTER :: tree_xy      
+      TYPE(kdtree2_result), ALLOCATABLE, DIMENSION(:) :: closest   
+      REAL(rp) :: xy(2)       
+      
+      INTEGER :: i,j,k,attr
+      INTEGER :: n,nattr
+      INTEGER :: nd
+      INTEGER :: clnd      
+      INTEGER :: nattr_list,attr_list(6)
+      INTEGER, DIMENSION(:), ALLOCATABLE :: neighbor_table
+      
+      old13%file_name = "/home/sbrus/data-drive/DWH_Oysters/grid/SL18TX33/sl18_tx_v2b.13.advectionstate.GULF"
+      old14%grid_file = "/home/sbrus/data-drive/DWH_Oysters/grid/SL18TX33/sl18_tx_v2c_071311.grd"
+      
+      prvi13%file_name = "/home/sbrus/data-drive/DWH_Oysters/grid/PRVI/PRVI-2017.13.V2"
+      prvi14%grid_file = "/home/sbrus/data-drive/DWH_Oysters/grid/PRVI/PRVI-2017.14"
+      
+      manning14%grid_file = "/home/sbrus/data-drive/DWH_Oysters/grid/SL18TX33+PRVI/SL18_GoM_PRVI_manning.grd"
+      submerge14%grid_file = "/home/sbrus/data-drive/DWH_Oysters/grid/SL18TX33+PRVI/SL18_GoM_PRVI_submergence.grd"
+      
+      new14%grid_file = "/home/sbrus/data-drive/DWH_Oysters/grid/SL18TX33+PRVI/SL18_GoM_PRVI_check.grd"
+      
+      ! average_horizontal_eddy_viscosity_in_sea_water_wrt_depth
+      ! sea_surface_height_above_geoid
+      ! surface_submergence_state
+      ! surface_canopy_coefficient
+      ! primitive_weighting_in_continuity_equation
+      ! surface_directional_effective_roughness_length
+      ! mannings_n_at_sea_floor
+      ! advection_state
+      
+      CALL read_fort13(old13)
+      CALL read_grid(old14,1)
+      CALL print_fort13_info(old13)      
+      
+      CALL read_fort13(prvi13)
+      CALL read_grid(prvi14,3)
+      CALL print_fort13_info(prvi13)      
+      
+      CALL read_grid(new14,3)      
+      CALL read_grid(manning14,1)
+      CALL read_grid(submerge14,1)
+   
 
       
-      old_grid%file_name = "/home/sbrus/data-drive/DWH_Oysters/grid/sl18_tx_v2b.13.advectionstate.GULF"
+      attr_list = (/ 1, 2, 5, 7, 3, 3 /)
       
-      CALL read_fort13(old_grid)
+      ALLOCATE(new13%defined_val(new14%nn,old13%n))
+      ALLOCATE(new13%default_val(old13%n))
+      ALLOCATE(new13%ndns(new14%nn,old13%n))
+      ALLOCATE(new13%nndns(old13%nattr))
+      ALLOCATE(new13%name(old13%nattr))
+      ALLOCATE(new13%units(old13%nattr))
+      ALLOCATE(new13%nvals(old13%nattr))
+      
+      nattr = 5
+      new13%nn = new14%nn
+      new13%n = nattr
+      new13%nattr = nattr
+      new13%file_name = "fort.13"
+      new13%grid_name = new14%grid_name    
+      
+      DO k = 1,nattr
+        j = attr_list(k)
+        i = old13%attr_index(j)        
+        new13%name(k)%line = old13%name(j)%line
+        new13%units(k)%line = old13%units(j)%line
+        new13%nvals(k) = old13%nvals(j)
+        new13%default_val(k) = old13%default_val(i)
+      ENDDO
+      
+      CALL apply_default_attributes(new13%nn,new13%nattr,new13%default_val,new13%defined_val)         
+      
+      CALL create_neighbor_table(old14%nn,old14%xy,new14%nn,new14%xy,neighbor_table)
+      
+      DO k = 1,3
+        j = attr_list(k)
+        i = old13%attr_index(j)
+        CALL nearest_neighbor_interpolation(new13%nn,neighbor_table,old13%defined_val(:,i),old13%default_val(i), &
+                                            new13%nndns(k),new13%ndns(:,k),new13%defined_val(:,k))
+      ENDDO
+      
+      PRINT*, "Finding non-default manning n values"
+      k = 4
+      j = attr_list(k)
+      i = old13%attr_index(j)      
+      CALL determine_non_default_nodes(new14%nn,manning14%depth,old13%default_val(i), &
+                                       new13%nndns(k),new13%ndns(:,k),new13%defined_val(:,k))                                                      
+      PRINT("(2(A,I9,2x))"), "non-default = ", new13%nndns(k), "default = ",new14%nn - new13%nndns(k)                                       
+      PRINT*, "" 
+      
+      PRINT*, "Finding non-default surface submergence state values"
+      k = 5
+      j = attr_list(k)
+      i = old13%attr_index(j) 
+      CALL determine_non_default_nodes(new14%nn,submerge14%depth,old13%default_val(i), &
+                                       new13%nndns(k),new13%ndns(:,k),new13%defined_val(:,k))                                                      
+      PRINT("(2(A,I9,2x))"), "non-default = ", new13%nndns(k), "default = ",new14%nn - new13%nndns(k)                                       
+      PRINT*, ""       
+      
+      CALL create_neighbor_table(prvi14%nn,prvi14%xy,new14%nn,new14%xy,neighbor_table)
+      
+      k = 6
+      j = attr_list(k)
+      i = prvi13%attr_index(j)
+      
+      DO nd = 1,new13%nn
+        new13%defined_val(nd,k) = prvi13%default_val(i)
+      ENDDO
+      
+      CALL nearest_neighbor_interpolation(new13%nn,neighbor_table,prvi13%defined_val(:,i),prvi13%default_val(i), &
+                                          new13%nndns(k),new13%ndns(:,k),new13%defined_val(:,k))
+                   
+      new13%n = new13%n + 1
+      new13%nattr = new13%nattr + 1                   
+      new13%name(k)%line = prvi13%name(i)%line
+      new13%units(k)%line = prvi13%units(i)%line
+      new13%nvals(k) = prvi13%nvals(i)
+      new13%default_val(k) = prvi13%default_val(i)                                          
 
-
+      CALL write_fort13(new13)
+      CALL print_fort13_info(new13)
+      
+!       PRINT*, new14%xy(1,new14%obnds(1,1)),new14%xy(2,new14%obnds(1,1))
+!       PRINT*, prvi14%xy(1,prvi14%obnds(1,1)),prvi14%xy(2,prvi14%obnds(1,1))      
 
       END PROGRAM create_fort13
