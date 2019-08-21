9a63cbe58e03a6dfadfe8a10fb003093290eb277
Steven Brus <steven.r.brus@gmail.com>
2019-08-21 10:45:03 -0600

    Add bathymetry images to examples

--- examples/galveston/clean_case.py
+++ examples/galveston/clean_case.py
@@ -8,4 +8,8 @@ subprocess.call(['rm *.ps'],shell=True)
 subprocess.call(['rm fort.*'],shell=True)
 subprocess.call(['rm -r PE*'],shell=True)
 subprocess.call(['rm np.in'],shell=True)
+subprocess.call(['rm dgswe_mpi'],shell=True)
+subprocess.call(['rm dgprep'],shell=True)
+subprocess.call(['rm dgpost'],shell=True)
+subprocess.call(['rm plot'],shell=True)
 
